18f48cdcccd96cf0bc0bfa8c149a093c3004cf6d
VAIO <ryanrodrigo880@gmail.com>
2026-07-01 14:42:01 -0300

    Meu terceiro envio

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 Lista = []
+
 while True:
     operacao = str(input("Digite I (para iniciar) ou S (para sair): "))
 
     if operacao == "I" or operacao == "i":
-        matricula = str(input("Digite o seu matricula: "))
+        matricula = int(input("Digite o seu matricula: "))
         aluno = str(input("Digite o seu aluno: "))
         nota1 = float(input("Digite sua nota 1: "))
         nota2 = float(input("Digite sua nota 2: "))
         media = (nota1 + nota2) / 2
         situacao = "Aprovado" if media >= 7 else "Reprovado"
 
-        dados = {matricula, aluno, media, situacao }
-
+        dados = {"matricula":matricula,
+                 "aluno":aluno,
+                 "media":media,
+                 "situacao":situacao }
         Lista.append(dados)
 
     elif operacao == "S" or operacao == "s":
         print("----------------FICHA----------------")
         print("MATRICULA | ALUNO  |  NOTA | SITUAÇÃO")
-        print(Lista)
+
+        for dados in Lista:
+            print(f'{dados["matricula"]}     |{dados["aluno"]}       | {dados["media"]}    |  {dados["situacao"]}')
 
         break
+
+    else:
+        print("invalido")
